13d6931f6b9e1c92723a4aa93c5a918f39b28d31
Sinclair Chen <abc.sinclair@gmail.com>
2024-06-05 17:43:31 -0700

    Add back about

---
+++ web/pages/about.tsx
@@ -0,0 +1,15 @@
+import { LovePage } from 'web/components/love-page'
+
+export default function About() {
+  return (
+    <LovePage trackPageView={'user profiles'}>
+      Manifold love development is on hiatus.
+      <br />
+      <br />
+      You answer questions and get a compatibility score.
+      <br />
+      Then you can like someone once a day to unlock the ability to message
+      them.
+    </LovePage>
+  )
+}
