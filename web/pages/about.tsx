007c72ea7e94a305cc6aa4d411e9b9d141f6ea6d
Sinclair Chen <abc.sinclair@gmail.com>
2025-03-05 21:03:25 -0800

    Update about - add discord

--- web/pages/about.tsx
+++ web/pages/about.tsx
@@ -3,14 +3,29 @@ import { LovePage } from 'web/components/love-page'
 export default function About() {
   return (
     <LovePage trackPageView={'user profiles'}>
-      Manifold love is under new construction. Expect incredible new things
-      soon. Hopefully.
+      🚧 🏗️ Manifold love is under new construction.
+      <br />
+      Expect incredible new things soon. 🚧 🏗️
+      <br />
+      Hopefully.
       <br />
       <br />
       You answer questions and get a compatibility score.
       <br />
       Then you can like someone once a day to unlock the ability to message
       them.
+      <br />
+      <br />
+      <span>
+        Join the{' '}
+        <a
+          href="https://discord.gg/AcCVcEEw7N"
+          className="hover:text-primary-700 cursor-pointer underline"
+        >
+          discord
+        </a>{' '}
+        to ask for help and give feedback.
+      </span>
     </LovePage>
   )
 }
