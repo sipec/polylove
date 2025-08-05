ef85bdbad324f3c46b9c38613faef4c35a302f1e
duck_master <duckeagle1280@gmail.com>
2025-08-05 03:22:43 -0400

    move jest.config.js to jest.config.mjs; run prettier again

--- common/eslint.config.mjs
+++ common/eslint.config.mjs
@@ -1,13 +1,12 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  { rules: { "@typescript-eslint/no-explicit-any": "off" } } // disable this particular rule for now since the `any` annotations are very tricky to get rid of
-];
+  { rules: { '@typescript-eslint/no-explicit-any': 'off' } }, // disable this particular rule for now since the `any` annotations are very tricky to get rid of
+]
