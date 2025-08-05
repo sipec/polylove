4eed39c76bc123b525b8437f43b2ed2e82ad4b6c
duck_master <duckeagle1280@gmail.com>
2025-08-05 03:45:28 -0400

    disable @typescript-eslint/no-unused-expressions

--- common/eslint.config.mjs
+++ common/eslint.config.mjs
@@ -8,5 +8,10 @@ export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  { rules: { '@typescript-eslint/no-explicit-any': 'off' } }, // disable this particular rule for now since the `any` annotations are very tricky to get rid of
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off', // disable this particular rule for now, since the `any` annotations are very tricky to get rid of
+      '@typescript-eslint/no-unused-expressions': 'off', // turn this off too for now, otherwise ESLint says "Cannot read properties of undefined"
+    },
+  },
 ]
