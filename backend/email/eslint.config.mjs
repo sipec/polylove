5de691e7f5cadd11caf5d38c3440e0248b9414dc
duck_master <duckeagle1280@gmail.com>
2025-08-06 02:33:10 -0400

    disable @typescript-eslint/no-unused-expressions for now

--- backend/email/eslint.config.mjs
+++ backend/email/eslint.config.mjs
@@ -8,4 +8,5 @@ export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  { rules: { '@typescript-eslint/no-unused-expressions': 'off' } },
 ]
